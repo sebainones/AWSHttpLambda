03587e1389ffb774d310ad5111701dd5818e8295
sebainones <seba.inones@gmail.com>
2021-12-08 11:26:29 +0100

    Added an input and adapted the test.

--- src/HttpLambda/Function.cs
+++ src/HttpLambda/Function.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.APIGatewayEvents;
 
+
+//[LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+//If you are using .NET Core 3.1, we recommend that you use the
+//Amazon.Lambda.Serialization.SystemTextJson serializer.
+//This package provides a performance improvement over Amazon.Lambda.Serialization.Json.
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
@@ -31,12 +36,12 @@ namespace HttpLambda
         /// </summary>
         /// <param name="context"></param>
         /// <returns></returns>
-        public APIGatewayProxyResponse FunctionHandler(ILambdaContext context)
+        public APIGatewayProxyResponse FunctionHandler(string input, ILambdaContext context)
         {
-            context.Logger.LogLine($"Processing HTPP Calllll...");
+            context.Logger.LogLine($"Processing HTPP Call for {input} ...");
             return new APIGatewayProxyResponse
             {
-                Body = "Hello Lambda, from Sebastian",
+                Body = $"Hello Lambda, from {input}",
                 StatusCode = 200
             };
         }
--- test/HttpLambda.Tests/FunctionTest.cs
+++ test/HttpLambda.Tests/FunctionTest.cs
@@ -7,23 +7,27 @@ namespace HttpLambda.Tests
     public class FunctionTest
     {
         [Fact]
-        public void TestToUpperFunction()
+        public void TestFunctionGetTheRightInput()
         {
 
             //Arrange
-            // Invoke the lambda function and confirm the string was upper cased.
+            // Invoke the lambda function and confirm the string was processed.
+
+            const string input = "SebasInput";
+
             var function = new Function();
             var context = new TestLambdaContext();
-            
+
             //Act
-            function.FunctionHandler(context);
+            var response = function.FunctionHandler(input, context);
 
 
             //Assert
             var testLogger = context.Logger as TestLambdaLogger;
 
             //Assert.Equal("HELLO WORLD");
-            Assert.Contains("Processing HTPP Calllll...", testLogger.Buffer.ToString());
+            Assert.Contains($"Processing HTPP Call for {input} ...", testLogger.Buffer.ToString());
+            Assert.Contains($"Hello Lambda, from {input}", response.Body);
         }
     }
 }
